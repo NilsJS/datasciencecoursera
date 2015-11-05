2fd451c1c3725aca61dea6e08f87b85563eaf534
Nils Sandøy <nils@NilsThinkPad.europe.corp.microsoft.com>
2015-11-05 16:26:18 +0100

    Started Data Products

---
+++ DataProducts/plotly/plot1.R
@@ -0,0 +1,13 @@
+library(plotly)
+set.seed(100)
+d <- diamonds[sample(nrow(diamonds), 1000), ]
+plot_ly(d, x = carat, y = price, text = paste("Clarity: ", clarity),
+        mode = "markers", color = carat, size = carat)
+
+
+# ggplot example
+p <- ggplot(data = d, aes(x = carat, y = price)) +
+  geom_point(aes(text = paste("Clarity:", clarity)), size = 4) +
+  geom_smooth(aes(colour = cut, fill = cut)) + facet_wrap(~ cut)
+
+(gg <- ggplotly(p))
