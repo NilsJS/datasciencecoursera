9c29f199ee61ecd3ec0148f8ca18a34e595d0ec7
Nils Sandøy <nils@NilsThinkPad.europe.corp.microsoft.com>
2015-11-17 08:16:52 +0100

    More project work

--- DataProducts/CourseProject/ShinyApp/server.R
+++ DataProducts/CourseProject/ShinyApp/server.R
@@ -1,18 +1,43 @@
-# http://data.worldbank.org/indicator/IT.CEL.SETS.P2/
 # http://www.magesblog.com/2013/02/first-steps-of-using-googlevis-on-shiny.html
 
-
-library(googleVis)
 library(shiny)
+# Rely on the 'WorldPhones' dataset in the datasets
+# package (which generally comes preloaded).
 library(datasets)
-library(dplyr)
 
-x <- data.frame()
-for(y in row.names(WorldPhones)) 
-    for(r in colnames(WorldPhones)) 
-        x <- rbind(x, cbind(Year=y, NumPhones=WorldPhones[y,r]*1000, Region=r))
-x$Year <- as.integer(x$Year)
-x$NumPhones <- as.integer(x$NumPhones)
+# Cell phones by country
+# http://data.worldbank.org/indicator/IT.CEL.SETS.P2/
+ph <- read.table("data/cellphones.csv", header=TRUE, skip = 4, sep = ',')
+
+# Population estimates
+# http://esa.un.org/unpd/wpp/DVD/
+pop <- read.csv("data/population.csv", header=TRUE)
+                                                                                        pop$Numerical.Country.Code <- pop$Country.Code
+
+# Mapping between different country codes
+# http://www.nationsonline.org/oneworld/country_code_list.htm
+cc <- read.csv("data/countries.csv", header=TRUE)
+
+# Define a server for the Shiny app
+shinyServer(function(input, output) {
+#  output$phonePlot <- reactive({input$Years})
+    output$phonePlot <- renderPlot({
+    barplot(WorldPhones[as.character(input$Year), ]*1000, 
+            main=input$Year,
+            ylab="Number of Telephones",
+            xlab="Year")
+  })
+})
+# library(googleVis)
+# library(datasets)
+# library(dplyr)
+# 
+# x <- data.frame()
+# for(y in as.numeric(row.names(WorldPhones)))
+#     for(r in colnames(WorldPhones)) 
+#         x <- rbind(x, cbind(Year=y, NumPhones=WorldPhones[as.factor(y),r]*1000, Region=r))
+#x$Year <- as.numeric(x$Year)
+#x$NumPhones <- as.integer(x$NumPhones)
 #                          WorldPhones[,input$region]*1000, 
 #                          main=input$region,
 #                          ylab="Number of Telephones",
@@ -23,37 +48,18 @@ x$NumPhones <- as.integer(x$NumPhones)
 #        "pressure" = pressure,
 #        "cars" = cars)
 
-wp <- data.frame(WorldPhones)
-
-shinyServer(function(input, output) {
-     datasetInput <- reactive({
-                      select(subset(x, Region==input$region), c('Year', 'NumPhones'))
-#                       main=input$region,
-#                       ylab="Number of Telephones",
-#                       xlab="Year"
-     })
-    
-    output$view <- renderGvis({
-        gvisScatterChart(datasetInput(), options=list(width=400, height=450))
-    })
-})
-
-
-
-# # Rely on the 'WorldPhones' dataset in the datasets
-# # package (which generally comes preloaded).
-# library(datasets)
-# 
-# # Define a server for the Shiny app
 # shinyServer(function(input, output) {
+#      datasetInput <- reactive({
+#                       select(subset(x, Region==input$region), c('Year', 'NumPhones'))
+# #                       main=input$region,
+# #                       ylab="Number of Telephones",
+# #                       xlab="Year"
+#      })
 #     
-#     # Fill in the spot we created for a plot
-#     output$phonePlot <- renderPlot({
-#         
-#         # Render a barplot
-#         barplot(WorldPhones[,input$region]*1000, 
-#                 main=input$region,
-#                 ylab="Number of Telephones",
-#                 xlab="Year")
+#     output$view <- renderGvis({
+#         gvisScatterChart(datasetInput(), options=list(width=400, height=450))
 #     })
 # })
+
+
+
--- DataProducts/CourseProject/ShinyApp/ui.R
+++ DataProducts/CourseProject/ShinyApp/ui.R
@@ -1,45 +1,49 @@
 library(shiny)
 
 # Rely on the 'WorldPhones' dataset in the datasets
-library(googleVis)
+#library(googleVis)
 library(datasets)
 
-shinyUI(pageWithSidebar(
-    headerPanel("Example 1: scatter chart"),
-    sidebarPanel(
-        selectInput("region", "Choose a region:", 
-                    choices = colnames(WorldPhones))
-    ),
-    mainPanel(
-        htmlOutput("view")
-    )
-))
+# shinyUI(pageWithSidebar(
+#     headerPanel("Example 1: scatter chart"),
+#     sidebarPanel(
+#         selectInput("Year", "Choose a Year:", 
+#                     choices = row.names(WorldPhones))
+#     ),
+#     mainPanel(
+#         # htmlOutput("phonePlot")
+#         plotOutput("phonePlot")
+#     )
+# ))
 
 # Define the overall UI
-# shinyUI(
-#     
-#     # Use a fluid Bootstrap layout
-#     fluidPage(    
-#         
-#         # Give the page a title
-#         titlePanel("Telephones by region"),
-#         
-#         # Generate a row with a sidebar
-#         sidebarLayout(      
-#             
-#             # Define the sidebar with one input
-#             sidebarPanel(
-#                 selectInput("region", "Region:", 
-#                             choices=colnames(WorldPhones)),
-#                 hr(),
-#                 helpText("Data from AT&T (1961) The World's Telephones.")
-#             ),
-#             
-#             # Create a spot for the barplot
-#             mainPanel(
-#                 plotOutput("phonePlot")  
-#             )
-#             
-#         )
-#     )
-# )
+shinyUI(
+    
+    # Use a fluid Bootstrap layout
+    fluidPage(    
+        
+        # Give the page a title
+        titlePanel("Telephones by region and year"),
+        
+        # Generate a row with a sidebar
+        sidebarLayout(      
+            
+            # Define the sidebar with one input
+            sidebarPanel(
+              # Simple integer interval
+#               sliderInput("Year", "Year:", 
+#                           min=1951, max=1961, value=1958),
+              selectInput("Year", "Year:", 
+                            choices=row.names(WorldPhones)),
+                hr(),
+                helpText("Data from AT&T (1961) The World's Telephones.")
+            ),
+            
+            # Create a spot for the barplot
+            mainPanel(
+                plotOutput("phonePlot")  
+            )
+            
+        )
+    )
+)
